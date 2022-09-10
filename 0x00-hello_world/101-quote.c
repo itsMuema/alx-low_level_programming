c928ac977a62b7bc9e778e483bceaafd5a94706c
itsMuema <smuema08@gmail.com>
2022-09-10 05:07:30 -0700

    Trailing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
- * This prints without printf and puts 
- * Return: Always 1 (Success)
+ * main - print quote
+ * Description: prints Dor's quote
+ * Return: Always 1 (success)
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		return (1);
 }
