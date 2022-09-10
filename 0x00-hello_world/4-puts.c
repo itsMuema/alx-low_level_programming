1169a326a561cc5f5587e5c9ed9e46a21c206845
itsMuema <smuema08@gmail.com>
2022-09-10 02:38:30 -0700

    First C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,6 @@
-#include <stdio.h>
-
-/**
- *  * main- Entry point
- *   * Description: A C program that prints with put function
- *    * Return: Always 0 (Success)
- *     */
-
+#include<stdio.h>
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
