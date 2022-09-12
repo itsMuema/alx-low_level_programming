805a5436019aec3932f209f5b7c899eaa8d1e8c9
itsMuema <smuema08@gmail.com>
2022-09-12 00:13:53 -0700

    Lower and Upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<Stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(0)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		putchar(low);
+	}
+	for (low = 'A'; low <= 'Z'; low++)
+	{
+		putchar(low);
+	}
+	putchar('\n');
+	return (0);
