4b2cc040f7afd4c9bd9e5a9c980f77f4e2ae4bdd
itsMuema <smuema08@gmail.com>
2022-09-12 01:16:59 -0700

    Without putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar("%d ", i);
+	}
+	putchar('\n');
+	return (0);
+}
