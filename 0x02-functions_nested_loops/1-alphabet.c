b1867ab6386e882f1bf0392eba78515e3ad9b1a8
itsMuema <smuema08@gmail.com>
2022-09-13 08:27:04 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0
+ */
+void print_alphabet(void);
+
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		_putchar(low);
+		_putchar('\n');
+	}
+	return (0);
+}
 
