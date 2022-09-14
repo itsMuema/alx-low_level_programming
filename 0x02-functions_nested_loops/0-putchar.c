6fa62b41272ba42b9388a4879b7f667fff6b019a
itsMuema <smuema08@gmail.com>
2022-09-14 13:25:32 -0700

    main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,6 +7,13 @@
  */
 int main(void)
 {
-	_putchar("_putchar\n");
+	char text[8] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
