5204ed1c627b3d9dcd691192485709ef02ff1c12
itsMuema <smuema08@gmail.com>
2022-09-14 23:50:14 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_sign - prints sign in relation with 0
+ *@c: an argument
+ *
+ *Return: 1 if > 0, 0 if == 0, -1 if < 0
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar("+");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar("0");
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar("-");
+		return (-1);
+	}
+}
