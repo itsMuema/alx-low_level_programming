9e8fa3d53942f1443f4d4bb77775db618b506b9a
itsMuema <smuema08@gmail.com>
2022-09-15 03:56:10 -0700

    Print numbers from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_to_98 - check main.h
+ *@n: Set integer
+ *
+ *Return: void
+ */
+void print_to_98(int n)
+{
+	for (n = 0; n <= 98; n++)
+	{
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+		
+	}
+
