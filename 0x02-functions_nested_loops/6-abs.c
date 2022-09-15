b58802f5f79ac0d0cae28dacad52e205242373aa
itsMuema <smuema08@gmail.com>
2022-09-15 00:21:47 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_abs - prints absolute value of integer
+ *@i : the integer variable
+ *
+ *Return: i if absolute else -i
+ */
+int _abs(int i)
+{
+	if (i > 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (-i);
+	}
+	return (0);
+}
