d74a0c5f020fd165fc136fcc9ad826e2b1c7775a
itsMuema <smuema08@gmail.com>
2022-09-15 03:17:16 -0700

    Adds 2 ints

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * add - add 2 integers
- *
- *Returns: result
+ * @a: int 1 declared
+ * @b: int 2 declared
+ * Return: result
  */
 int add(int a, int b)
 {
 	int result;
 
 	result = a + b;
-	return (result):
+	return (result);
 }
