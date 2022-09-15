71c8378626fdfa6ea9967db0864422c505bc747a
itsMuema <smuema08@gmail.com>
2022-09-15 01:35:33 -0700

    Jack Baeur

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#incude "main.h"
+/**
+ * jack_bauer - print every minute of the day
+ *
+ *Return: Nothing
+ *
+ */
+void jack_bauer(void)
+{
+	int hours;
+	int minutes;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (minutes = 0; minutes < 60; minutes++)
+		{
+			_putchar(hours / 10 + '0');
+			_putchar(hours % 10 + '0');
+			_putchar(':');
+			_putchar(minutes / 10 + '0');
+			_putchar(minutes % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
