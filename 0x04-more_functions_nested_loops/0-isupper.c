eb8f1b985996f553a7de6c3a440b3379e58f247b
itsMuema <smuema08@gmail.com>
2022-09-16 14:12:53 -0700

    Upper Case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_isupper - checks if c i uppercase
+ *
+ *Return: 1 if uppercase,otherwise 0
+ */
+int _isupper(int c);
+{
+	for(c = 'A'; c <= 'Z'; c++)
+	{
+		if (c = 'A' && c = 'Z')
+		{
+			return (1)
+		}
+		else
+		{
+			return (0)
+		}
+	}
+}
