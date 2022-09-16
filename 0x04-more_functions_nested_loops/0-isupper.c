5aea6a1a1640e4bb12528965264698e5ec27161f
itsMuema <smuema08@gmail.com>
2022-09-16 14:22:35 -0700

    Upper Case

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
  *_isupper - checks if c i uppercase
- *
+ *@c: character variable
  *Return: 1 if uppercase,otherwise 0
  */
 int _isupper(int c);
 {
-	for(c = 'A'; c <= 'Z'; c++)
+	for (c = 'A'; c <= 'Z'; c++)
 	{
-		if (c = 'A' && c = 'Z')
+		if (c == 'A' && c == 'Z')
 		{
-			return (1)
+			return (1);
 		}
 		else
 		{
-			return (0)
+			return (0);
 		}
 	}
 }
