d51b9a240d9b6513699e10cae4eeb89084bbedf3
itsMuema <smuema08@gmail.com>
2022-09-16 01:53:50 -0700

    Correction 0

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include"main.h"
 /**
  * main - Entry point
  *
@@ -12,6 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 2)
 	{
 		printf("%d is positive\n", n);
