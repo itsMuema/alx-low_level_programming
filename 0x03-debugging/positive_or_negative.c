cf128952f7e13b9da5eb1e0999b81b505476fd2c
itsMuema <smuema08@gmail.com>
2022-09-16 04:49:56 -0700

    Correction 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - evaluates whether a value n is positive or negative
+ *
+ * Description: program allocates a random number to the
+ * value n and evaluates whether it is positive or negative
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 2)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
