7afa898f2ec463137996f6f569dfc894a7f11473
itsMuema <smuema08@gmail.com>
2022-09-20 03:28:54 -0700

    Resets to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets vaue of int
+ * @i: variable to be reset
+ *
+ *Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	int i;
+
+	i = 360;
+	*n = &i;
+
+
+	_putchar("Value of 'i':%d\n", i);
+	*n = 98;
+	_putchar("Now reset to:%d\n",i);
+	return;
+}
+
+
