d7441869de76e374a616859ae4109b1f169e01b5
itsMuema <smuema08@gmail.com>
2022-09-22 12:14:18 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
- *  _strncpy - a function that copys the strings.
- *  @dest: an input string
- *  @src: an input string
- *  @n: an input integer
+ * _strncpy - a function that copys the strings.
+ * @dest: an input string
+ * @src: an input string
+ * @n: an input integer
  *
- *
- *  Return: A pointer to the resulting string
+ * Return: A pointer to the resulting string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
