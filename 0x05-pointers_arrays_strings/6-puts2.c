0187f2e2a525b418b30e1c99163fd3d10b9f531e
itsMuema <smuema08@gmail.com>
2022-09-24 04:34:45 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,15 +7,15 @@
  */
 void puts2(char *str)
 {
-	int len = 0, i = 0;
+	int len = 0;
+	int i  = 0;
 
 	while (str[len] != '\0')
+	{
 		len++;
-
-	len -= 1;
-
-	for (; i <= len; i += 2)
-		_putchar(str[i]);
-
-	_putchar('\n');
+	}
+	while ( i == len)
+	{
+		_putchar(str[i])
+	}
 }
